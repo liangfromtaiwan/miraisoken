a4e3e4d77078609c6ddaba3592ea7194ad4eea5e
リャン <92705187+liangfromtaiwan@users.noreply.github.com>
2023-07-18 18:45:44 +0900

    ㄉ

--- フューチャリスト＿0711/asset/js/all.js
+++ フューチャリスト＿0711/asset/js/all.js
@@ -1,17 +1,13 @@
-// $(document).ready(function () {
-//   $(".faq__content__list__item").on("click", function () {
-//       $(".faq__content__list__item").removeClass("active"),
-//       $(this).addClass("active");
-//   });
-// });
-
 //入塾までの流れについてに戻る
 $(document).ready(function() {
   $("#toStep").on("click", function() {
     $("html, body").animate({
       scrollTop: $("#step").offset().top - 100
     }, 500);
-   $(this).parent().parent().parent().parent().find("#step .faq__content__list__item__a").slideDown(1000);
+  $(this).parent().parent().parent().parent().find
+  ("#step .faq__content__list__item__a").slideDown(1000);
+  $(this).parent().parent().parent().parent().find
+  ("#step").addClass("active");
   });
 });
 
@@ -19,24 +15,28 @@ $(document).ready(function() {
 $(document).ready(function () {
   $(".faq__content__list--active .faq__content__list__item").click(function (e) { 
     e.preventDefault();
-   $(this).find(".faq__content__list__item__a").slideToggle();
-   $(this).siblings().find(".faq__content__list__item__a").slideUp();
-   $(this).parent().siblings().find(".faq__content__list__item__a").slideUp();
-   $(this).addClass(active);
-  }); 
+    $(this).find(".faq__content__list__item__a").slideToggle();
+   $(this).toggleClass("active");
+  });
 });
 $(document).ready(function () {
   $(".faq__content__list .faq__content__list__item").click(function (e) { 
     e.preventDefault();
-   $(this).find(".faq__content__list__item__a").slideToggle();
-  }); 
-});
-$(document).ready(function () {
-  $(".faq__content__list__item").on("click", function () {
-      $(".faq__content__list__item").removeClass("active"),
-      $(this).addClass("active");
+    $(this).find(".faq__content__list__item__a").slideToggle();
+    $(this).toggleClass("active");
   });
 });
 
 
-
+//ページ内リンクのスムーズスクロールを実装
+// スムーズスクロール
+$(function () {
+    $('a[href^="#"]:not(a.lightbox)').click(function () {
+        var speed = 300;
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top - 30; // 60pxの余白をつける
+        $('html,body').animate({ scrollTop: position }, speed, 'swing');
+        return false;
+    });
+});
