e8ee4a0eb14e9309a6a1d7d6cf0d62229821112a
Liang <92705187+liangfromtaiwan@users.noreply.github.com>
2023-07-19 15:14:36 +0900

    Update all.js

--- フューチャリスト＿0711/asset/js/all.js
+++ フューチャリスト＿0711/asset/js/all.js
@@ -11,14 +11,14 @@ $(document).ready(function () {
 
 //faq slide down
 $(document).ready(function () {
-  $(".faq__content__list--active .faq__content__list__item").click(function (e) {
+  $(".faq__content__list--active .faq__content__list__item__q").click(function (e) {
     e.preventDefault();
     $(this).find(".faq__content__list__item__a").slideToggle();
     $(this).toggleClass("active");
   });
 });
 $(document).ready(function () {
-  $(".faq__content__list .faq__content__list__item").click(function (e) {
+  $(".faq__content__list .faq__content__list__item__q").click(function (e) {
     e.preventDefault();
     $(this).find(".faq__content__list__item__a").slideToggle();
     $(this).toggleClass("active");
